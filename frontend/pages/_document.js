5b8030b3bfffdbfbc0afc7841db6476a88cfb774
Thales Avila <dev.thales.avila@gmail.com>
2021-02-07 14:31:46 -0300

    feat: implementing NProgress component in NextJS

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-br">
-        {/* <Head></Head> */}
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
