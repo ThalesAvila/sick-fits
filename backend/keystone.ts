0be1e62308ded73e02c1a9f995bdc46bbeb68ca3
Thales Avila <dev.thales.avila@gmail.com>
2021-02-07 19:54:01 -0300

    chore: setting up Keystone and TS

---
+++ backend/keystone.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL = process.env.DATABASE_URL || '';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({}),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO Add session values here
+});
