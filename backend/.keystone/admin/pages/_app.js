23a5f4fbb20105e91a0c0a63c2b1fb9657dca463
Thales Avila <dev.thales.avila@gmail.com>
2021-02-12 11:50:49 -0300

    feat: adding Auth to our Application

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -7,10 +7,11 @@ import { Core } from '@keystone-ui/core';
 import * as viewb59481e1 from '../../../node_modules/@keystone-next/fields/types/mongoId/views';
 import * as view74d0825c from '../../../node_modules/@keystone-next/fields/types/text/views';
 import * as view8ea50b39 from '../../../node_modules/@keystone-next/fields/types/password/views';
+import * as viewefae7a80 from '../../../node_modules/@keystone-next/fields/types/timestamp/views';
 
 const adminConfig = {};
 
-const fieldViews = { viewb59481e1, view74d0825c, view8ea50b39 };
+const fieldViews = { viewb59481e1, view74d0825c, view8ea50b39, viewefae7a80 };
 
 const lazyMetadataQuery = {
   kind: 'Document',
@@ -150,6 +151,29 @@ const lazyMetadataQuery = {
             },
             loc: { start: 22, end: 234 },
           },
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'authenticatedItem' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                {
+                  kind: 'InlineFragment',
+                  typeCondition: {
+                    kind: 'NamedType',
+                    name: { kind: 'Name', value: 'User' },
+                  },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
         ],
       },
     },
@@ -161,7 +185,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="1f29un9"
+        adminMetaHash="qqeudb"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+
 
 const databaseURL = process.env.DATABASE_URL || '';
 
@@ -9,23 +12,41 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-  },
-  lists: createSchema({
-    User
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO Add session values here
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles
+  }
 });
+
+export default withAuth(
+  // @ts-ignore
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User
+    }),
+    ui: {
+      // Show the ui only to people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id name email`
+    })
+  })
+);
