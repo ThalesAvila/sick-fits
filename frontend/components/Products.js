5ff5381bc85fc68329e60e533ccdd98a22c17b55
Thales Avila <dev.thales.avila@gmail.com>
2021-02-21 16:02:08 -0300

    feat: hooking ALL_PRODUCTS_QUERY to Productst component

---
+++ frontend/components/Products.js
@@ -0,0 +1,27 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data, error, loading);
+  return <div>Products</div>;
+}
+
+export default Products;
