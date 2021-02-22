3878bc3964319964c198761f12774c6f359d990a
Thales Avila <dev.thales.avila@gmail.com>
2021-02-22 16:05:25 -0300

    feat: adding checksd for error and loading in Product Component

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -21,6 +21,9 @@ const ALL_PRODUCTS_QUERY = gql`
 function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   console.log(data, error, loading);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return <div>Products</div>;
 }
 
